e0199d584e50f23f563f7fbe3f427d540ef632e9
Prabhat <parbhatkushwaha5@gmail.com>
2025-01-24 02:45:23 +0530

    dynamic keyword

---
+++ lib/dynamicKeyword.dart
@@ -0,0 +1,8 @@
+void main(){
+  dynamic a = 10;
+  print(a);
+  a = 'rahul';
+  print(a);
+  a = 5.5;
+  print(a);
+}
--- lib/stringToInt.dart
+++
@@ -1,6 +0,0 @@
-void main(){
-  int a = 10;
-  int b = int.parse('20');
-  print(a);
-  print(b);
-}
