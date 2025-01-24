2fa97c82b78116cdca6d3a6af2afc5a415aba248
Prabhat <parbhatkushwaha5@gmail.com>
2025-01-24 12:55:25 +0530

    final and const keyword

---
+++ lib/dart Practice/const_final.dart
@@ -0,0 +1,14 @@
+void main(){
+ /*
+ this show error because final keyword declared only one time
+
+  final a = 10;
+  a = 20;
+
+  */
+  final a = 10;
+  print(a);
+
+  const b = 10;
+  print(b);
+}
